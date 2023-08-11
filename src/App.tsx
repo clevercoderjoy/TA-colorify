9ff10d3e705c3bc305414c75f851c0a5e3521b37
clevercoderjoy <clevercoderjoy@gmail.com>
2023-08-12 00:25:27 +0530

    initial-setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HeaderComponent from "./components/headerComponent/HeaderComponent";
+
+function App() {
+  return (
+    <>
+      <HeaderComponent />
+    </>
+  );
+}
+
+export default App;
