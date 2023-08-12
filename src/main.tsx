4d0161ee4ba00dd3db93efc2f2981b54af5534d9
clevercoderjoy <clevercoderjoy@gmail.com>
2023-08-12 16:09:10 +0530

    feat: shape component

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,5 @@ import "./global-styles/globals.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
